ca5f7907805fb1ff80f7f91c0702b43f01ca0441
DeeptiWani <wanideepti44@gmail.com>
2024-07-15 21:50:21 +0530

    Initial Commit

---
+++ src/bloodbankmanagementsystem/BloodBankManagementSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bloodbankmanagementsystem;
+
+/**
+ *
+ * @author Dell
+ */
+public class BloodBankManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
